ec9ce67b6d500d19883e8c6432daf7618b27403c
Sylvain Drexler W. <96838061+drex25@users.noreply.github.com>
2025-06-27 09:50:06 -0300

    agregar normativas (#15)
    Co-authored-by: Sylvain Drexler Wilvins <tsgroup420@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import IngresosBrutos from './pages/IngresosBrutos';
 import ImpuestoInmobiliario from './pages/ImpuestoInmobiliario';
 import ControlFiscal from './pages/ControlFiscal';
 import TasasYAranceles from './pages/TasaYAranceles';
+import Normativas from './pages/Normativas';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
             <Route path="/tramites/inmobiliario" element={<ImpuestoInmobiliario />} />
             <Route path="/tramites/control-fiscal" element={<ControlFiscal/>} />
             <Route path="/tramites/tasas-aranceles" element={<TasasYAranceles/>} />
+            <Route path="/informacion-fiscal/normativas" element={<Normativas/>} />
           </Routes>
         </main>
         <Footer />
